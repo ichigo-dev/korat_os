dd1baaf845807885a55c468dea35a1e4ffe3fc1f
ichigo <dev.honda.ichigo@gmail.com>
2022-11-18 11:32:23 +0900

    feat: Complete the test framewrok

--- dev/korat_os/src/main.rs
+++ dev/korat_os/src/main.rs
@@ -6,13 +6,12 @@
 
 #![no_std]
 #![no_main]
+
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(korat_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
+use korat_os::println;
 use core::panic::PanicInfo;
 
 //------------------------------------------------------------------------------
@@ -46,47 +45,5 @@ fn panic( info: &PanicInfo ) -> !
 #[panic_handler]
 fn panic( info: &PanicInfo ) -> !
 {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode
-{
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu( exit_code: QemuExitCode )
-{
-    use x86_64::instructions::port::Port;
-
-    unsafe
-    {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-fn test_runner( tests: &[&dyn Fn()] )
-{
-    serial_println!("\nRunning {} tests", tests.len());
-    for test in tests
-    {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion()
-{
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
+    korat_os::test_panic_handler(info);
 }
